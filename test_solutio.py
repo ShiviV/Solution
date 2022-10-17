1a8da7a4e4421fce2ae119f411a7cf940f1c4720
Shivangi Verma <vhshlsimmi@gmail.com>
2022-10-18 01:39:30 +0800

    New

--- test_solutio.py
+++ test_solutio.py
@@ -12,26 +12,26 @@ def test_answer():
     v = [[2, 1, 0, 2, 1],
          [0, 0, 1, 2, 1],
          [1, 0, 0, 2, 1]]
-    assert Solution.rotOranges(v) == -1 
+    assert solution.rotOranges(v) == -1 
     print("Success")
 
 def test_answer_all_zeroes():
     v = [[0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0]]
-    assert Solution.rotOranges(v) == 0 
+    assert solution.rotOranges(v) == 0 
 
 def test_answer_all_ones():
     v = [[1, 1, 1, 1, 1],
          [1, 1, 1, 1, 1],
          [1, 1, 1, 1, 1]]
-    assert Solution.rotOranges(v) == -1 
+    assert solution.rotOranges(v) == -1 
 
 def test_size():
     v = [[2, 1, 0, 2, 1],
          [0, 0, 1, 2, 1],
          [1, 0, 0, 2, 1]]
-    assert Solution.checksize(v) !=(1001,1001)
+    assert solution.checksize(v) !=(1001,1001)
     print("Success")
 
 
